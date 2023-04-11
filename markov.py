e6b70b8079e302095d6a5aa91b40899fb716d83b
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-11 13:51:15 -0700

    completed the Markov Chains for Green Eggs & Ham

--- markov.py
+++ markov.py
@@ -10,13 +10,6 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
-    # 1. open file input_path
-    # 2. read a file as one string of context
-        # save the file-string as a new variable
-    # 3. close file
-    # 4. return one string
-
     open_file = open(file_path)
     contents = open_file.read()
     open_file.close()
@@ -50,36 +43,8 @@ def make_chains(text_string):
     """
 
     chains = {}
-
-    # your code goes here
-    # 1. split the string so each word is its own string
-    # 2. making a loop and a new list variable as bigram, 
-    #    pick two words as a tuple from a line and put into a new variable
-    #   have to identify possible words as values, too?
     
-    words = text_string.split()
-
-    # possible_words = []
-    # while..?
-    # chains.get()
-    ## From the hint
-        # 1. Make empty dict
-        # 2. Loop over the words in the list of words, making sure can access
-            # at i, i+1, and i+2 without returning IndexError
-        # 3. Modify loop so the words at i and i+1 become a tuple
-            # use the tuple as the key in our dictionary
-        # 4. create a list as the pair value and append words into it
-        # 5. When do we make the empty list and append into it?
-            # a. check if they key is in the dict already
-            # b. if not, initialize that list and add word to it
-            # c. if key is in dict, append word to the list that already exists
-    # chains[(word1, word2)] = []
-
-    # for i in range(len(words) - 3):
-
-    #     links = chains.get((word1, word2), [])
-    #     links.append(word3)
-    #     chains[(word1, word2)] = links
+    words = text_string.split() 
 
     for i in range(len(words) - 2):
         word1 = words[i]
@@ -99,36 +64,22 @@ def make_chains(text_string):
 def make_text(chains):
     """Return text from chains."""
 
-    words = []
+    key_list = list(chains.keys())
+    key = (choice(key_list))
+    word = choice(chains[key])
+    output_string = [word]
 
-    # your code goes here
-    # link = key (tuple) + one random word from value (list)
-    # 1. choose a key randomly throght import choice from random module
-        # 1. making a list of chains's keys => [(), (), (), ()]
-    # 2. choose randomly same as step 1.
-    # 3. till the end point.(end point => the key which has one word)
-    # 4. join together as a string.
-    # while loop?
+    while True:
+        key = (key[1], word)
 
-    chains_key_list = list(chains.keys())
-    # all keys from our chains dictionary
-    start_key = choice(chains_key_list)
-    # a starting key
-    random_word = choice(chains[start_key])
-    # a random word from the starting key-value
-    new_key = start_key[1], random_word
-    # a new key of second word from start_key, random_word
-    # i = 0
-    # while i < 3:
-        
-    # new_key = first_key
-    # random_word = choice(chains[(new_key)])
-    # first_key = chains[(new_key, random_word)]
+        if key in key_list:
+            word = choice(chains[key])
+            output_string.append(word)
+        else:
+            break
     
     
-    # return start_key, random_word, new_key
-
-    # return ' '.join(words)
+    return ' '.join(output_string)
 
 
 input_path = 'green-eggs.txt'
@@ -138,9 +89,10 @@ input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
 chains = make_chains(input_text)
+# print(chains)
 
 # Produce random text
 random_text = make_text(chains)
-
-print(input_text)
-print(chains)
+print(random_text)
+# print(input_text)
+# print(chains)
